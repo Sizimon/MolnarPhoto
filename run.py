6bdd95cf5a1a1c0ee36bbb21c0baf2aac84a6588
Sizimon <siizmonsus@gmail.com>
2023-01-17 00:32:02 +0000

    FEAT: Ensure routing is working correctly with Flask

--- run.py
+++ run.py
@@ -1,11 +1,24 @@
+# IMPORTS
 import os
-from flask import Flask
+from flask import Flask, render_template
+
+# FLASK TEMPLATE SETUP
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
